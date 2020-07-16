351bf383405149bbdfd47a02b163e78fdf7b5bdd
18chowdhary <schowdhary@olin.edu>
2020-07-16 10:21:25 -0400

    Update footer, formatting might be a little weird

---
+++ src/components/AgencyGrid.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function AgencyGrid(props) {
+    let styleMods = props.bg + " " + props.text;
+    let footerClassName = "usa-footer__secondary-section" + styleMods;
+    let gridClassName = "grid-cotainer footer-padding " + styleMods;
+
+    return(
+    <div class={footerClassName}>
+        <div class={gridClassName}>
+            <div class="agency-grid footer-text">
+                <img class="agency-item agency-adjustment-first" src={props.logo1} alt=""/>
+
+                <div class="agency-item">
+                    <p>{props.logo2text}</p>
+                    <img src={props.logo2} alt=""/>
+                </div>
+                        
+                <img class="agency-item agency-adjustment" src={props.logo3} alt=""/>
+
+                <div class="agency-item">
+                    <p>{props.logo4text}</p>
+                    <img src={props.logo4} alt=""/>
+                </div> 
+            </div>
+
+            <p/>
+                
+            <p class="footer-text">{props.trademarkText}</p>
+        </div>
+    </div>
+    );
+}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,54 +4,30 @@ import UnityLogo from '../assets/img/nyc_unity_logo.png';
 import CYELogo from '../assets/img/NYCYE.png';
 import MayorsFundLogo from '../assets/img/mayorsfund.png';
 import MOCTOLogo from '../assets/img/MOCTO.png';
+import AgencyGrid from './AgencyGrid';
 
 export default function Footer(props) {
     let returnBgClass = "grid-container usa-footer__return-to-top "+props.returnBg;
     let footerBgClass = "usa-footer "+props.returnBg;
     return(
-        <div>
-            <footer class={footerBgClass}>
-                <div class={returnBgClass}>
-                    <a href="#">Return to top</a>
-                </div>
-                
-                <FooterNav
-                link1="/" label1="Home"
-                link2="/" label2="About"
-                link3="/" label3="Contact"
-                link4="/" label4="Privacy Policy"
-                link5="/" label5="Terms of Use"/>
+        <footer class={footerBgClass}>
+            <div class={returnBgClass}>
+                <a href="#">Return to top</a>
+            </div>
+            
+            <FooterNav
+            link1="/" label1="Home"
+            link2="/" label2="About"
+            link3="/" label3="Contact"
+            link4="/" label4="Privacy Policy"
+            link5="/" label5="Terms of Use"/>
 
-                <div class="usa-footer__secondary-section bg-unity-purple text-white ">
-                    <div class="grid-container bg-unity-purple text-white">
-                    <div class="agency-grid footer-text">
-                                    <img class="agency-item agency-adjustment-first" src={UnityLogo} alt=""/>
+            <AgencyGrid bg="bg-unity-purple" text="text-white" logo1={UnityLogo}
+            logo2text="A collaboration of" logo2={CYELogo}
+            logo3={MayorsFundLogo}
+            logo4text="Website created with the support of" logo4={MOCTOLogo}
+            trademarkText={["© 2020 City of New York. All Rights Reserved.",<br/>,"NYC is a trademark and service mark of the City of New York"]}/>
 
-                                    <div class="agency-item">
-                                        <p>A collaboration of</p>
-                                        <img src={CYELogo} alt=""/>
-                                    </div>
-                                    
-                                    <img class="agency-item agency-adjustment" src={MayorsFundLogo} alt=""/>
-
-                                    <div class="agency-item">
-                                        <p>Website created with support of</p>
-                                        <img src={MOCTOLogo} alt=""/>
-                                    </div> 
-                                </div>
-                            {/* <div class="usa-footer__contact-links mobile-lg:grid-col-6">
-                                
-                            </div> */}
-
-                            <p/>
-                            
-                            <p class="footer-text">© 2020 City of New York. All Rights Reserved.
-                            <br/>NYC is a trademark and service mark of the City of New York</p>
-                        <div class="grid-row grid-gap">
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </div>     
+        </footer>   
     );
 }
