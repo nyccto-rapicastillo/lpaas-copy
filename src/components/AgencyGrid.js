bdf3ec0903c612ce1d005ec8f791e95c1989d1d3
18chowdhary <schowdhary@olin.edu>
2020-07-16 16:23:45 -0400

    Attempt to fix footer

--- src/components/AgencyGrid.js
+++ src/components/AgencyGrid.js
@@ -8,26 +8,30 @@ export default function AgencyGrid(props) {
     return(
     <div class={footerClassName}>
         <div class={gridClassName}>
-            <div class="agency-grid footer-text">
-                <img class="agency-item agency-adjustment-first" src={props.logo1} alt=""/>
+            <div class="grid-row grid-gap">
+                <div class="agency-grid footer-text">
+                        <img class="agency-item agency-adjustment-first" src={props.logo1} alt=""/>
 
-                <div class="agency-item">
-                    <p>{props.logo2text}</p>
-                    <img src={props.logo2} alt=""/>
-                </div>
+                        <div class="agency-item">
+                            <p>{props.logo2text}</p>
+                            <img src={props.logo2} alt=""/>
+                        </div>
+                                
+                        <img class="agency-item agency-adjustment" src={props.logo3} alt=""/>
+
+                        <div class="agency-item">
+                            <p>{props.logo4text}</p>
+                            <img src={props.logo4} alt=""/>
+                        </div> 
+                    </div>
+
+                    <p/>
                         
-                <img class="agency-item agency-adjustment" src={props.logo3} alt=""/>
+                    <p class="footer-text">{props.trademarkText}</p>
 
-                <div class="agency-item">
-                    <p>{props.logo4text}</p>
-                    <img src={props.logo4} alt=""/>
-                </div> 
-            </div>
-
-            <p/>
+                </div>
                 
-            <p class="footer-text">{props.trademarkText}</p>
-        </div>
+            </div>
     </div>
     );
 }
