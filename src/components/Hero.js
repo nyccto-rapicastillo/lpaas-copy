618df4e14c3a228bbdb31a7f09b5ecde4a617f43
18chowdhary <schowdhary@olin.edu>
2020-07-17 14:25:37 -0400

    Create new classes for barlow font loading

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,19 +4,17 @@ import Button from './Button';
 export default function Hero(props) {
     let imgClassName = "usa-hero "+props.bgImg
     return(
-        <div>
-            <section class={imgClassName} aria-label="Introduction">
-                <div class="grid-container">
-                    <div class="usa-hero__callout">
-                        <h1 class="usa-hero__heading">
-                            <span class="usa-hero__heading--alt font-barlow-logo text-unity-blue">Work It, NYC
-                            <br/><span class="text-accent-cool">A Guide to LGBTQI+ Workplace Inclusivity</span> 
-                            </span>
-                        </h1>
-                        <Button buttonActive={true} buttonLink={props.link} buttonText="Download Report"/>
-                    </div>
+        <section class={imgClassName} aria-label="Introduction">
+            <div class="grid-container">
+                <div class="usa-hero__callout">
+                    <h1 class="usa-hero__heading">
+                        <span class="usa-hero__heading--alt font-barlow-logo text-unity-blue">Work It, NYC
+                        <br/><span class="text-accent-cool">A Guide to LGBTQI+ Workplace Inclusivity</span> 
+                        </span>
+                    </h1>
+                    <Button buttonActive={true} buttonLink={props.link} buttonText="Download Report"/>
                 </div>
-            </section>
-        </div>     
+            </div>
+        </section>  
     );
 }
--- src/components/YellowSection.js
+++ src/components/YellowSection.js
@@ -5,12 +5,15 @@ export default function YellowSection(props) {
         <div>
             <section class="grid-container usa-section">
                 <div class="grid-row grid-gap">
+
                     <div class="tablet:grid-col-4">
-                        <h2 class="font-heading-xl margin-top-0 tablet:margin-bottom-0">{props.heading}</h2>
+                        <h2 class="font-heading-xl margin-top-0 tablet:margin-bottom-0 font-barlow-heading text-primary-darker">{props.heading}</h2>
                     </div>
+
                     <div class="tablet:grid-col-8 usa-prose">
                         <p>{props.text}</p>
                     </div>
+                    
                 </div>
             </section>
         </div>     
