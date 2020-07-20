8a4ed1ea61a81ef6eaeedb2c02c556c14615175d
18chowdhary <schowdhary@olin.edu>
2020-07-20 14:34:20 -0400

    Fix font styling in Blue Section

--- src/components/BlueSection.js
+++ src/components/BlueSection.js
@@ -6,7 +6,7 @@ export default function BlueSection(props) {
         <section id="test-section-id" class="usa-section bg-unity-blue">
             <div class="grid-container">
                 <h2 class="font-heading-xl margin-y-0 text-barlow-heading text-primary-darker">{props.heading}</h2>
-                <p class="usa-intro text-public-sans">{props.text}</p>
+                <p class="usa-intro usa-prose">{props.text}</p>
                 <Button buttonActive={props.buttonActive} buttonText={props.buttonText} buttonLink={props.buttonLink}/>
             </div>
         </section>     
