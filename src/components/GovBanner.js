aa125fff3744b7722e3d9f3be154f1222756eba2
18chowdhary <schowdhary@olin.edu>
2020-07-23 15:31:46 -0400

    Update gov banner

--- src/components/GovBanner.js
+++ src/components/GovBanner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NYCLogo from '../assets/img/nyc.png';
+import NYC from '../assets/img/nyc.svg';
 
 import {useTranslation} from 'react-i18next';
 import {LANGUAGES} from '../i18n/config';
@@ -13,11 +14,14 @@ export default function GovBanner() {
       <section className="usa-banner bg-primary-darker"
         aria-label="Official government website">
         <div className="usa-accordion">
-          <header className="usa-banner__header">
+          <header className="usa-banner__header" style={{height: 22}}>
             <div className="usa-banner__inner">
               <div className="grid-col-fill tablet:grid-col-auto">
                 <p className="usa-banner__header-text text-white">
-                  <img src={NYCLogo} style={{paddingRight: 50}}/> {t('topNav')}
+                  <a href="http://nyc.gov/">
+                    <img src={NYC} style={{paddingRight: 28}}/>
+                  </a> 
+                  {t('topNav')}
                 </p>
               </div>
             </div>
