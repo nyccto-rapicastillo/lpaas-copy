d8097930df768b2bd35d7e6595a5ba31958526b6
18chowdhary <schowdhary@olin.edu>
2020-07-30 12:18:19 -0400

    Fix bugs

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default function Button(props) {
-  return <a className="usa-button usa-button--big"
+  return <a className="usa-button usa-button--big" {...props}
     href={props.link}>{props.children}</a>;
 }
